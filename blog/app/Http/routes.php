b9015114bbeaeadcc4097139e971e15ce73b5540
Maiducthach <=>
2016-06-02 14:45:48 +0700

    List Controller

--- blog/app/Http/routes.php
+++ blog/app/Http/routes.php
@@ -1,16 +1,26 @@
 <?php
+Route::group(['prefix'=>'admin'],function(){
+    Route::group(['prefix'=>'news'],function(){
+        Route::resource('category','News\NewsCategoryController');
+        Route::resource('list','News\NewsListController');
+        
+    });
+    Route::group(['prefix'=>'product'],function(){
+        Route::resource('category','Product\ProductCategoryController');
+        Route::resource('list','Product\ProductListController');
+        Route::resource('attribute','Product\ProductAttributeController');
+    });
+    Route::group(['prefix'=>'order'],function(){
+        Route::resource('customer','Order\CustomerController');
+        Route::resource('invoice','Order\InvoiceController');
+        Route::resource('order','Order\OrderController');
+        Route::resource('payment','Order\PaymentController');
+        Route::resource('shipment','Order\ShipmentController');
+    });
+});
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
 
 Route::get('/', function () {
+    
     return view('welcome');
 });
